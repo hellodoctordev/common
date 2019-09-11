f11d56cdbd3eae5750c83bf630f6d4100a492a92
Daniel Sarfati <daniel@hellodoctor.com.mx>
2019-09-11 11:11:26 -0500

    fixup! fixup! fixup! fixup! fixup! use mapclaims

--- keys/loader.go
+++ keys/loader.go
@@ -1,39 +1,53 @@
 package keys
 
 import (
+	"cloud.google.com/go/storage"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
 	"reflect"
 	"strings"
 )
 
 func Load(keyType KeyType, env string) {
-	appRoot, err := os.Getwd()
+	keyFileData, err := getKeyFileData(keyType)
 	if err != nil {
-		panic(err)
-	}
-
-	keysRoot := fmt.Sprintf("%s/.keys", appRoot)
-	keysRoot = "/keys"
-	keyFile := fmt.Sprintf("%s/%s.%s.keys", keysRoot, keyType.GetKeyFilePrefix(), env)
-
-	v := reflect.ValueOf(keyType).Elem()
-
-	keyFileData, err := ioutil.ReadFile(keyFile)
-	if err != nil {
-		log.Printf("error occurred reading keyFile %s: %s", keyFile, err)
+		log.Printf("error occurred reading keyFileData for '%s': %s", keyType, err)
 		return
 	}
 
+	keyTypeValue := reflect.ValueOf(keyType).Elem()
+
 	keys := strings.Split(string(keyFileData), "\n")
+
 	for _, key := range keys {
 		if len(key) == 0 {
 			continue
 		}
 
 		keyData := strings.Split(key, "=")
-		v.FieldByName(keyData[0]).SetString(keyData[1])
+		keyTypeValue.FieldByName(keyData[0]).SetString(keyData[1])
 	}
+}
+
+func getKeyFileData(keyType KeyType) (data []byte, err error) {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	env := "stage"
+	keyObjectName := fmt.Sprintf("%s.%s.keys", keyType.GetKeyFilePrefix(), env)
+
+	rc, err := client.Bucket("hellodoctor-staging-keys").Object(keyObjectName).NewReader(ctx)
+	if err != nil {
+		return
+	}
+
+	defer rc.Close()
+
+	return ioutil.ReadAll(rc)
 }
