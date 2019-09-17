dc8ada803338695617537159fc00686a3946af00
Daniel Sarfati <daniel@hellodoctor.com.mx>
2019-09-17 13:05:47 -0500

    fixup! added common scheduling types

--- types/scheduling.go
+++ types/scheduling.go
@@ -24,6 +24,38 @@ func NewInterval(start time.Time, end time.Time) (interval Interval, err error) 
 	return
 }
 
+func (i *Interval) Duration() time.Duration {
+	return i.End.Sub(i.Start)
+}
+
+func (i *Interval) Equal(i2 Interval) bool {
+	return i.Start.Equal(i2.Start) && i.End.Equal(i2.End)
+}
+
+func (i *Interval) Abuts(i2 Interval) bool {
+	return i.Start.Equal(i2.End) || i.End.Equal(i2.Start)
+}
+
+func (i *Interval) Contains(i2 Interval) bool {
+	return (i.Start.Before(i2.Start) || i.Start.Equal(i2.Start)) && (i.End.After(i2.End) || i.End.Equal(i2.End))
+}
+
+func (i *Interval) Overlaps(i2 Interval) bool {
+	if i.Equal(i2) || i.Contains(i2) || i2.Contains(*i) {
+		return true
+	}
+
+	if i.Abuts(i2) {
+		return false
+	}
+
+	return i.Start.Equal(i2.Start) && i.End.Before(i2.End) ||
+		i.Start.After(i2.Start) && i.End.Equal(i2.End) ||
+		i.Start.Before(i2.Start) && i.End.After(i2.Start) ||
+		i2.Start.Before(i.Start) && i2.End.After(i.Start)
+}
+
+
 type ConsultationRequest struct {
 	ConsultationID   string   `json:"consultationID"`
 	ConsultationType string   `json:"consultationType"`
