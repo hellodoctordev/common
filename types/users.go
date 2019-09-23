14429ebcb42036e697dd8d8c001c6c7b5ade4dcf
Daniel Sarfati <daniel@hellodoctor.com.mx>
2019-09-23 13:11:02 -0500

    add json

--- types/users.go
+++ types/users.go
@@ -1,25 +1,25 @@
 package types
 
 type FirestoreBillingSettingsConsultationPrice struct {
-	Amount   int64  `firestore:"amount"`
-	Currency string `firestore:"currency"`
+	Amount   int64  `firestore:"amount",json:"amount"`
+	Currency string `firestore:"currency",json:"currency"`
 }
 
 type FirestoreBillingSettingsConsultation struct {
-	BillingType string `firestore:"billingType"`
-	IsEnabled   bool   `firestore:"isEnabled"`
+	BillingType string `firestore:"billingType",json:"billingType"`
+	IsEnabled   bool   `firestore:"isEnabled",json:"isEnabled"`
 }
 
 type FirestoreBillingSettings struct {
-	ChatConsultations  FirestoreBillingSettingsConsultation `firestore:"chatConsultations"`
-	VoiceConsultations FirestoreBillingSettingsConsultation `firestore:"voiceConsultations"`
-	VideoConsultations FirestoreBillingSettingsConsultation `firestore:"videoConsultations"`
+	ChatConsultations  FirestoreBillingSettingsConsultation `firestore:"chatConsultations",json:"chatConsultations"`
+	VoiceConsultations FirestoreBillingSettingsConsultation `firestore:"voiceConsultations",json:"voiceConsultations"`
+	VideoConsultations FirestoreBillingSettingsConsultation `firestore:"videoConsultations",json:"videoConsultations"`
 }
 
 type FirestoreBilling struct {
-	Billing FirestoreBillingSettings `firestore:"billing"`
+	Billing FirestoreBillingSettings `firestore:"billing",json:"billing"`
 }
 
 type FirestoreDoctorUser struct {
-	Billing FirestoreBilling `firestore:"billing"`
+	Billing FirestoreBilling `firestore:"billing",json:"billing"`
 }
