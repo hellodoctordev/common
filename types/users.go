6333e957ceb989e74a8d27e2d5ae76fa0a371aa9
Daniel Sarfati <daniel@hellodoctor.com.mx>
2019-09-23 13:15:35 -0500

    fixup! fixup! typo

--- types/users.go
+++ types/users.go
@@ -1,26 +1,26 @@
 package types
 
 type FirestoreBillingSettingsConsultationPrice struct {
-	Amount   int64  `firestore:"amount",json:"amount"`
-	Currency string `firestore:"currency",json:"currency"`
+	Amount   int64  `firestore:"amount" json:"amount"`
+	Currency string `firestore:"currency" json:"currency"`
 }
 
 type FirestoreBillingSettingsConsultation struct {
-	BillingPrice FirestoreBillingSettingsConsultationPrice `firestore:"billingPrice",json:"billingPrice"`
-	BillingType  string                                    `firestore:"billingType",json:"billingType"`
-	IsEnabled    bool                                      `firestore:"isEnabled",json:"isEnabled"`
+	BillingPrice FirestoreBillingSettingsConsultationPrice `firestore:"billingPrice" json:"billingPrice"`
+	BillingType  string                                    `firestore:"billingType" json:"billingType"`
+	IsEnabled    bool                                      `firestore:"isEnabled" json:"isEnabled"`
 }
 
 type FirestoreBillingSettings struct {
-	ChatConsultations  FirestoreBillingSettingsConsultation `firestore:"chatConsultations",json:"chatConsultations"`
-	VoiceConsultations FirestoreBillingSettingsConsultation `firestore:"voiceConsultations",json:"voiceConsultations"`
-	VideoConsultations FirestoreBillingSettingsConsultation `firestore:"videoConsultations",json:"videoConsultations"`
+	ChatConsultations  FirestoreBillingSettingsConsultation `firestore:"chatConsultations" json:"chatConsultations"`
+	VoiceConsultations FirestoreBillingSettingsConsultation `firestore:"voiceConsultations" json:"voiceConsultations"`
+	VideoConsultations FirestoreBillingSettingsConsultation `firestore:"videoConsultations" json:"videoConsultations"`
 }
 
 type FirestoreBilling struct {
-	Settings FirestoreBillingSettings `firestore:"settings",json:"settings"`
+	Settings FirestoreBillingSettings `firestore:"settings" json:"settings"`
 }
 
 type FirestoreDoctorUser struct {
-	Billing FirestoreBilling `firestore:"billing",json:"billing"`
+	Billing FirestoreBilling `firestore:"billing" json:"billing"`
 }
