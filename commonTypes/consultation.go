d527ded16f8fe64d0d5e52c0de16cf0e54802cd9
Daniel Sarfati <daniel@hellodoctor.com.mx>
2019-10-01 12:33:09 -0500

    fixup! fixup! fixup! moving a lot of things around. hoooo boy

--- commonTypes/consultation.go
+++ commonTypes/consultation.go
@@ -2,6 +2,7 @@ package commonTypes
 
 import "cloud.google.com/go/firestore"
 
+// TODO Perhaps get rid of this entirely
 type ConsultationDocument struct {
 	Participants    []*firestore.DocumentRef `firestore:"participants"`
 	ParticipantUIDs []string                 `firestore:"_participantUIDs"`
