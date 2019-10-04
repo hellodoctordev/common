06737d8622db8604863922748b746b3dc83a2e05
Daniel Sarfati <daniel@hellodoctor.com.mx>
2019-10-04 08:13:51 -0500

    added WriteJSONResponse

--- utils/requests.go
+++ utils/requests.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"github.com/hellodoctordev/common/logging"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -20,4 +21,16 @@ func ReadRequestBody(r *http.Request, o interface{}) (err error) {
 	}
 
 	return
+}
+
+func WriteJSONResponse(w http.ResponseWriter, response interface{}) {
+	js, err := json.Marshal(response)
+	if err != nil {
+		logging.Error("could not marshal CreateChatConsultationResponse: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(js)
 }
