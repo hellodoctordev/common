820979734ad1810e6fbb19767c674fae040f038e
Daniel Sarfati <daniel@hellodoctor.com.mx>
2019-11-05 14:15:27 +0200

    fixup! added google maps client

--- clients/google.go
+++ clients/google.go
@@ -1,12 +1,13 @@
 package clients
 
 import (
+	"github.com/hellodoctordev/common/keys"
 	"github.com/hellodoctordev/common/logging"
 	"googlemaps.github.io/maps"
 )
 
 func NewGoogleMapsClient() *maps.Client {
-	client, err := maps.NewClient(maps.WithAPIKey("Insert-API-Key-Here"))
+	client, err := maps.NewClient(maps.WithAPIKey(keys.GoogleApiKeys.GoogleApiServerKey))
 	if err != nil {
 		logging.Error("could not create google maps api client: %s", err)
 		return nil
