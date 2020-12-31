d310cee35ed538e42c827c062a896200a7c6b7e6
Daniel Sarfati <daniel@hellodoctor.com.mx>
2020-12-31 17:29:12 -0600

    added billing client

---
+++ clients/billing.go
@@ -0,0 +1,42 @@
+package clients
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type BillingClient struct {
+	HttpServiceClient
+}
+
+func NewBillingClient() *BillingClient {
+	serviceHost := os.Getenv("BILLING_SERVICE_HOST")
+
+	if len(serviceHost) == 0 {
+		serviceHost = DefaultServiceHost
+	}
+
+	return &BillingClient{
+		HttpServiceClient{
+			Client:      http.DefaultClient,
+			ServiceHost: serviceHost,
+		},
+	}
+}
+
+type CreateAuthorizedChargeRequest struct {
+	PaymentMethodID      string `json:"paymentMethodID"`
+}
+
+type CreateAuthorizedChargeResponse struct {
+	PaymentIntentID string `json:"paymentIntentID"`
+}
+
+func (client *BillingClient) CreateAuthorizedCharge(consultationID, paymentMethodID string) (*http.Response, error) {
+	req := CreateAuthorizedChargeRequest{
+		PaymentMethodID:paymentMethodID,
+	}
+
+	return client.Post(fmt.Sprintf("/charges/consultations/%s/_authorize", consultationID), req)
+}
